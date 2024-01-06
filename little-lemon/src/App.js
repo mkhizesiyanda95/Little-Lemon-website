fe388f8cc5f0e56137f5ec11594d50e251f14b5a
mkhizesiyanda95 <150601546+mkhizesiyanda95@users.noreply.github.com>
2024-01-06 09:30:05 +0200

    first commit

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Home page
-    </div>
+    <>
+      <h1>
+      Meta Frontend Developer Capstone Starter Files
+      </h1>
+    </>
   );
 }
 
---
+++ little-lemon/src/components/Nav.js
@@ -0,0 +1,17 @@
+import  React from "react";
+
+const Nav = () => {
+    return(
+        <nav>
+            <a href="/">
+                <img src={logo} alt="logo"/>
+            </a>
+
+            <div>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
